d38e6f53665b4c7a2bfd08283526883b8660f5b3
Guilherme <guilhermebugs2001@gmail.com>
2023-07-05 20:34:06 -0300

    refact/style/ correção cart e estilo home

---
+++ src/components/shoppCart/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+import { letterParagraph, letterPriceSmall, letterTitle4 } from "../../styles/globalStyle.ts";
+
+
+export const UlStyled = styled.ul`
+    
+`
