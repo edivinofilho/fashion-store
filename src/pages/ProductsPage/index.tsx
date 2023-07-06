1e3c9fc21efab85d6db6634db968f0888cace0c7
felipogit <86251880+felipogit@users.noreply.github.com>
2023-07-05 21:49:11 -0300

    estilização

--- src/pages/ProductsPage/index.tsx
+++ src/pages/ProductsPage/index.tsx
@@ -1,11 +1,11 @@
- import { FooterDefault } from "../../components/Footer/index.tsx"
+import { FooterDefault } from "../../components/Footer/index.tsx"
 import { HeaderDefault } from "../../components/Header/index.tsx"
 import { ProductContext } from "../../providers/ProductsContext/ProductsContex.tsx";
-import { ConteinerTopStyled } from "./style.ts"
+import { ConteinerTopStyled, ProductMainStyled } from "./style.ts"
 import { useContext } from 'react';
 import { IProduct } from "../../providers/ProductsContext/@types.ts";
-import cart from "../../assets/cart.svg"
-import { StyledProductList } from "../../styles/UlStyled.ts"; 
+import cart from "../../assets/cartPlus.svg"
+import { StyledProductList } from "../../styles/UlStyled.ts";
 import { ProductItem } from "../../components/ProductItem/index.tsx";
 
 export const ProductsPage = () => {
@@ -14,26 +14,31 @@ export const ProductsPage = () => {
   const filterProductList = productList?.filter(product => product.id !== currentProduct?.id)
 
   return (
-  
-   <ConteinerTopStyled>
+
+    <ConteinerTopStyled>
       <HeaderDefault onlyBrand={false} />
-      <div>
-        <span>PRODUTO PRINCIPAL</span>
-        <img src={currentProduct?.image} />
-        <p>{currentProduct?.name}</p>
-        <p>R$ {currentProduct?.price}</p>
+      <main>
+      <div className="HomeProduct" ><h3 >Home &gt; {currentProduct?.name}</h3></div>
+      <ProductMainStyled>
         <div>
-          <img src={cart} alt="Carrinho" />
-          <span>Saiba Mais</span>
+          <img src={currentProduct?.image} />
         </div>
-      </div>
+        <div>
+          <h4>{currentProduct?.name}</h4>
+          <span>{currentProduct?.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</span>
+          <p>{currentProduct?.description}</p>
+          <button> <img src={cart} alt="Carrinho" /> Adicionar Ao carrinho</button>
+        </div>
+      </ProductMainStyled>
+      <h2>Veja Também</h2>
       <StyledProductList styledDiv="otherPage" >
         {
-          filterProductList?.map(item => <ProductItem key={item.id} item={item}/>)
-           
+          filterProductList?.map(item => <ProductItem key={item.id} item={item} />)
+
         }
       </StyledProductList>
+      </main>
       <FooterDefault />
-    </ConteinerTopStyled> 
+    </ConteinerTopStyled>
   )
 }
