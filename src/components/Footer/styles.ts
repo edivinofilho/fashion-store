8185939fb529de9032b5e61bd78aa4ae25d2b818
edivinofilho <edivinofilho@hotmail.com>
2023-07-07 21:11:31 -0300

    fix: correções theme-Footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -7,8 +7,8 @@ export const FooterStyled = styled.footer`
     width: 100%;
     min-height: 120px;
     height: auto;
-    /* background-color: ${({theme}) => theme.colors.black};
-    color: ${({theme}) => theme.colors.white}; */
+    background-color: #000000;
+    color: #FFFFFF;
     display: flex;
     justify-content: center;
     align-items: center;
